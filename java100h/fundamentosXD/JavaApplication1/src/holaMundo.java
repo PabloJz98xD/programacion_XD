57d0b6b77426bb595a851179e3dbb2e73597e44a
PabloJz98xD <juan-pablo-z-s58@hotmail.com>
2021-05-11 06:22:20 -0500

    Fundamentos Java 1

--- java100h/fundamentosXD/JavaApplication1/src/holaMundo.java
+++ java100h/fundamentosXD/JavaApplication1/src/holaMundo.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 //Mi clase en Java 
 public class holaMundo {
     public static void main(String[] args) {
@@ -21,5 +24,42 @@ public class holaMundo {
         var miVariable=1;
         var _miVariable=2;
         var $miVariable=3;
+        //Concatenacion en Java Ejemplo 1:
+        var usuario = "Juan";
+        var titulo = "Ingeniero";
+        var union = titulo + " " + usuario;
+        System.out.println("union = " + union);
+        //Concatenacion en Java Ejemplo 2:
+        var i = 3;
+        var j = 4;
+        System.out.println(i + j);//Se realiza la suma de numeros.
+        System.out.println(i + j + usuario);//Evaluacion de izq a der, realiza suma.
+        System.out.println(usuario + i + j);//Contexto cadena, todo es una cadena.
+        System.out.println(usuario + (i + j));//Uso de parentesis modifican la prioridad en la evaluacion.
+        
+        //Caracteres especiales en Java
+        var nombre = "Pablo";
+        System.out.println("Nueva linea: \n"+nombre);
+        System.out.println("Tabulador: \t" + nombre);
+        System.out.println("Retroceso: \b"+nombre);
+        System.out.println("Comilla simple: \'"+nombre+"\'");
+        System.out.println("Comilla doble: \""+nombre+"\"");
+        
+        //Clase Scanner
+        System.out.println("Escribe tu nombre: ");
+        Scanner sc = new Scanner (System.in);
+        var usuario2 = sc.nextLine();
+        System.out.println("Usuario = "+usuario2);
+        System.out.println("Escribe titulo: ");
+        var titulo2 = sc.nextLine();
+        System.out.println("Resultado: "+titulo2+" "+usuario2);
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Proporciona el numero1:");
+        int numero1 = Integer.parseInt(scanner.nextLine());
+        System.out.println("Proporciona el numero2:");
+        int numero2 = Integer.parseInt(scanner.nextLine());
+        System.out.println("El numero mayor es:");
+        System.out.println(numero1 > numero2 ? numero1 : numero2);
     }
 }
